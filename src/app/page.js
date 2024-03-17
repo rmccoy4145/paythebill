369774b25c7b0e89da5673d68747a78fceda9648
rmccoy4145 <ryan.mccoy85@gmail.com>
2024-03-16 21:40:48 -0400

    add useEffect deps, style srollbar for bills

--- src/app/page.js
+++ src/app/page.js
@@ -23,11 +23,10 @@ import {
   DialogTrigger,
   DialogClose, DialogFooter,
 } from "@/components/ui/dialog"
-import {Label} from "@/components/ui/label";
 import { Input } from "@/components/ui/input"
 
 export default function Home() {
-
+  const [refreshCount, setRefreshCount] = useState(0);
   const [dueDates, setDueDates] = useState([]);
   const [scheduledBills, setScheduledBills] = useState([]);
   const [currentBills, setCurrentBills] = useState([]);
@@ -87,7 +86,7 @@ export default function Home() {
           setNoBills(true);
         }
     }
-  }, []);
+  }, [refreshCount]);
 
   // const handleDayClick = (day, modifiers) => {
   //   const newSelectedDays = [...days];
@@ -103,6 +102,9 @@ export default function Home() {
   // };
 
   const saveNewBill = () => {
+    if (newScheduledBillValue.name === "" || newScheduledBillValue.amount === "" || newScheduledBillValue.dayDue === "") {
+      return;
+    }
     const dueDate = createDateFromDay(newScheduledBillValue.dayDue);
     const newScheduledBill = {
       id: uuidv4(),
@@ -125,6 +127,7 @@ export default function Home() {
     if (typeof window !== "undefined") {
       localStorage.setItem(localstorageBillsKey, JSON.stringify(updatedScheduledBills));
     }
+    setRefreshCount(refreshCount + 1);
   };
 
   const markBillPaid = (newPaidBill) => {
@@ -134,6 +137,7 @@ export default function Home() {
     if (typeof window !== "undefined") {
       localStorage.setItem(localstoragePaidBillsKey, JSON.stringify(updatedPaidBills));
     }
+    setRefreshCount(refreshCount + 1);
   };
 
   const handleAddBillInputChange = (e) => {
@@ -160,17 +164,18 @@ export default function Home() {
           </div>
           <div className="flex flex-row w-full gap-x-32 justify-center">
             <div className="flex flex-col">
+              <div className="flex flex-row w-full justify-center p-4">
+                <p>Due Date Calendar</p>
+              </div>
               <Calendar
-                  className="rounded-md border w-72"
+                  className="rounded-md border w-72 animate-fade-in-once"
                   inputProps={{ readOnly: true, disabled: true }}
-                  // onDayClick={handleDayClick}
-                  mode="multiple"
                   selected={dueDates}
               />
             </div>
-            <div className="flex flex-col">
-              <div className="flex flex-row w-full gap-x-8 justify-between items-center pb-4">
-              <p>This Month's Bills</p>
+            <div className="flex flex-col gap-y-3 h-96 overflow-y-scroll">
+              <div className="flex flex-row w-full justify-between items-center pb-2 pr-5">
+              <p>Bills for the Month</p>
               <AddBillButton inputHandler={handleAddBillInputChange} submitHandler={saveNewBill} />
               </div>
               {noBills ? <NoBillsCard/> :
@@ -180,8 +185,7 @@ export default function Home() {
                     bill={bill}
                     payHandler={() => markBillPaid(bill)}
                 />
-              ))
-              :   <LoadingSpinner/>
+              )) :   <LoadingSpinner/>
               }
             </div>
           </div>
@@ -192,7 +196,7 @@ export default function Home() {
 
 function BillCard({bill, payHandler}) {
   return (
-    <Card className="min-w-96">
+    <Card className="min-w-96 animate-fade-in-once mr-5">
       <CardTitle>
       {bill.name}
       </CardTitle>
@@ -211,7 +215,7 @@ function BillCard({bill, payHandler}) {
 
 function NoBillsCard() {
   return (
-      <Card className="min-w-96">
+      <Card className="min-w-96 animate-fade-in-once mr-5">
         <CardTitle>
           No Bills Due
         </CardTitle>
@@ -224,7 +228,7 @@ function NoBillsCard() {
 
 function LoadingSpinner() {
   return (
-      <div className="min-w-96 justify-self-center">
+      <div className="min-w-96 min-h-64 justify-self-auto">
         <svg aria-hidden="true"
              className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
              viewBox="0 0 100 101" fill="none"
@@ -236,24 +240,7 @@ function LoadingSpinner() {
               d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
               fill="currentFill"/>
         </svg>
-        Processing...
       </div>);
-}
-
-function formatDate(date) {
-  console.log(date)
-  return date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-}
-
-function createDateFromDay(day) {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = now.getMonth();
-  return new Date(year, month, day);
 }
 
 function AddBillButton({inputHandler, submitHandler}) {
@@ -274,13 +261,32 @@ function AddBillButton({inputHandler, submitHandler}) {
               <Input onChange={inputHandler} placeholder="Name" name="name"/>
               <Input onChange={inputHandler} placeholder="Amount" name="amount"/>
               <Input onChange={inputHandler} placeholder="Day Due" name="dayDue"/>
-            <Button onClick={submitHandler} variant="secondary" type="submit" size="sm" className="px-3 w-1/3">
-              Add
-            </Button>
+              <DialogTrigger asChild>
+                <Button onClick={submitHandler} variant="secondary" type="submit" size="sm" className="px-3 w-1/3">
+                  Add
+                </Button>
+              </DialogTrigger>
             </div>
           </div>
         </DialogContent>
       </Dialog>
   )
+}
+
+// helper functions
+function formatDate(date) {
+  console.log(date)
+  return date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+}
+
+function createDateFromDay(day) {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  return new Date(year, month, day);
 }
 
